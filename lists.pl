ac4c2159076e34173ee6506b2ab56055ab8e7bd5
Finn van Montfort <74103207+TheCheesyWiggle@users.noreply.github.com>
2023-11-07 16:22:20 +0000

    Update lists.pl

--- lists.pl
+++ lists.pl
@@ -14,9 +14,16 @@ max_list([X|XS], V) :-
 member_list(E,[E|_]).
 member_list(E, [_|XS]) :-
     member_list(E, XS).
+
+reverse_list([],[]).
+reverse_list([X|XS],W) :-
+    reverse_list(XS,V),
+    append(V, [X], W).
+    
     
 
 main :-
     %sum_list([2,3,5,6],X), write(X),
-	  %max_list([2,3,5,6],X), write(X),
-    member_list(d, [a,b,c,d,e]).
+	%max_list([2,3,5,6],X), write(X),
+    %member_list(d, [a,b,c,d,e]),
+    reverse_list([2,3,4,5,6],X),write(X).
