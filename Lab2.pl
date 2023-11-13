f3cae22ac0f77522f4af0ce2a449196221f253fc
Finn van Montfort <74103207+TheCheesyWiggle@users.noreply.github.com>
2023-11-13 14:55:25 +0000

    Update Lab2.pl

--- Lab2.pl
+++ Lab2.pl
@@ -2,3 +2,9 @@ length_list([],0).
 length_list([_|T],W):-
     length_list(T,V),
     W is V+1.
+
+take_list(_,[],[]).
+take_list(0,_,[]).
+take_list(N, [X|XS], [X|W]):-
+   	N1 is N-1,
+    take_list(N1, XS, W).
