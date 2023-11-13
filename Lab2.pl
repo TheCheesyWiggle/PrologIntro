d4f4a75e5eadfff128a3d885bebeb4c4afe0f9e5
Finn van Montfort <74103207+TheCheesyWiggle@users.noreply.github.com>
2023-11-13 15:24:19 +0000

    Update Lab2.pl

--- Lab2.pl
+++ Lab2.pl
@@ -8,3 +8,28 @@ take_list(0,_,[]).
 take_list(N, [X|XS], [X|W]):-
    	N1 is N-1,
     take_list(N1, XS, W).
+
+drop_list(_,[],[]).
+drop_list(0,X,X).
+drop_list(N, [_|XS], W):-
+    N1 is N-1,
+    drop_list(N1, XS, W).
+
+lickety_split(_,[],[],[]).
+lickety_split(0,X,[],X).
+lickety_split(N, [X|XS], [X|AS], BS):-
+    N1 is N-1,
+    lickety_split(N1, XS, AS, BS ).
+
+squares([],[]).
+squares([X,XS], [W|WS]):-
+    squares(XS,WS),
+    W is X*X.
+
+evens([],[]).
+evens([X|XS],W):-
+    1 is X mod 2,
+    evens(XS, W).
+evens([X|XS],[X|W]):-
+    0 is X mod 2,
+    evens(XS, W).
